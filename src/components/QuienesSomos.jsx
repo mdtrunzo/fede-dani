417d920977fb5806b5177fe11616249ba722166d
Matias Daniel Trunzo <matiasdanieltrunzo@MacBook-Pro-de-Matias.local>
2022-07-01 19:13:59 +0200

    video 6

--- src/components/QuienesSomos.jsx
+++ src/components/QuienesSomos.jsx
@@ -5,6 +5,7 @@ import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew'
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
 import { Element } from 'react-scroll'
+import video6 from '../assets/6.mp4'
 
 function QuienesSomos() {
   const [iconItems, setIconItems] = useState({
@@ -31,87 +32,91 @@ function QuienesSomos() {
     setIconItems(iconItems)
   }, [iconItems])
 
-  const loadCanvas = () => {
-    const stars = document.getElementById('stars')
-    const starsCtx = stars.getContext('2d')
+//   const loadCanvas = () => {
+//     const stars = document.getElementById('stars')
+//     const starsCtx = stars.getContext('2d')
 
-    let screen,
-      starsElements,
-      starsParams = { speed: 0.2, number: 350, extinction: 4 }
+//     let screen,
+//       starsElements,
+//       starsParams = { speed: 0.2, number: 350, extinction: 4 }
 
-    // run stars
-    setupStars()
-    updateStars()
+//     // run stars
+//     setupStars()
+//     updateStars()
 
-    // update stars on resize to keep them centered
-window.onresize = function() {
-  setupStars();
-};
-// star constructor
-function Star() {
-  this.x = Math.random() * stars.width;
-  this.y = Math.random() * stars.height;
-  this.z = Math.random() * stars.width;
+//     // update stars on resize to keep them centered
+// window.onresize = function() {
+//   setupStars();
+// };
+// // star constructor
+// function Star() {
+//   this.x = Math.random() * stars.width;
+//   this.y = Math.random() * stars.height;
+//   this.z = Math.random() * stars.width;
 
-  this.move = function() {
-      this.z -= starsParams.speed;
-      if (this.z <= 0) {
-          this.z = stars.width;
-      }
-  };
+//   this.move = function() {
+//       this.z -= starsParams.speed;
+//       if (this.z <= 0) {
+//           this.z = stars.width;
+//       }
+//   };
 
-  this.show = function() {
-      let x, y, rad;
-      x = (this.x - screen.c[0]) * (stars.width / this.z);
-      x = x + screen.c[0];
-      y = (this.y - screen.c[1]) * (stars.width / this.z);
-      y = y + screen.c[1];
-      rad = stars.width / this.z;
+//   this.show = function() {
+//       let x, y, rad;
+//       x = (this.x - screen.c[0]) * (stars.width / this.z);
+//       x = x + screen.c[0];
+//       y = (this.y - screen.c[1]) * (stars.width / this.z);
+//       y = y + screen.c[1];
+//       rad = stars.width / this.z;
 
 
-      starsCtx.beginPath();
-      starsCtx.fillStyle = "#538691";
-      starsCtx.arc(x, y, rad, 0, Math.PI * 2);
-      starsCtx.fill();
-  }
-}
+//       starsCtx.beginPath();
+//       starsCtx.fillStyle = "#538691";
+//       starsCtx.arc(x, y, rad, 0, Math.PI * 2);
+//       starsCtx.fill();
+//   }
+// }
 
-// setup <canvas>, create all the starts
-function setupStars() {
-  screen = {
-      w: window.innerWidth,
-      h: window.innerHeight,
-      c: [ window.innerWidth * 0.5, window.innerHeight * 0.5 ]
-  };
-  window.cancelAnimationFrame(updateStars);
-  stars.width = screen.w;
-  stars.height = screen.h;
-  starsElements = [];
-  for (let i = 0; i < starsParams.number; i++) {
-      starsElements[i] = new Star();
-  }
-}
+// // setup <canvas>, create all the starts
+// function setupStars() {
+//   screen = {
+//       w: window.innerWidth,
+//       h: window.innerHeight,
+//       c: [ window.innerWidth * 0.5, window.innerHeight * 0.5 ]
+//   };
+//   window.cancelAnimationFrame(updateStars);
+//   stars.width = screen.w;
+//   stars.height = screen.h;
+//   starsElements = [];
+//   for (let i = 0; i < starsParams.number; i++) {
+//       starsElements[i] = new Star();
+//   }
+// }
 
-// redraw the frame
-function updateStars() {
-  starsCtx.fillStyle = "white";
-  starsCtx.fillRect(0, 0, stars.width, stars.height);
-  starsElements.forEach(function (s) {
-      s.show();
-      s.move();
-  });
-  window.requestAnimationFrame(updateStars);
-}
-  }
+// // redraw the frame
+// function updateStars() {
+//   starsCtx.fillStyle = "white";
+//   starsCtx.fillRect(0, 0, stars.width, stars.height);
+//   starsElements.forEach(function (s) {
+//       s.show();
+//       s.move();
+//   });
+//   window.requestAnimationFrame(updateStars);
+// }
+//   }
 
-  useEffect(() => {
-    loadCanvas()
-  }, [])
+  // useEffect(() => {
+  //   loadCanvas()
+  // }, [])
 
   return (
     <Element id='quienes-somos'>
     <div className="quienes-somos red">
-      <canvas id="stars"></canvas>
+
+          <video loop autoPlay muted>
+            <source src={video6} type="video/mp4" />
+          </video>
+      {/* <canvas id="stars"></canvas> */}
       <Fade>
         <div className="quienes-somos-content">
           <h3 className="upper-title pb-20">CONOCENOS</h3>
